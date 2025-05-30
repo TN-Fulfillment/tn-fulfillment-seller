ac2843c65ee0cc7330ee5e6acd40fc53d71cfd59
dattranit124 <60561454+dattranit124@users.noreply.github.com>
2025-05-30 15:48:20 +0700

    update

---
+++ nuxt.config.ts
@@ -0,0 +1,53 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+import tailwindcss from "@tailwindcss/vite";
+export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "TN Fulfillment Solution For POD & Dropship Seller",
+      meta: [
+        // <meta name="description" content="My amazing site">
+        {
+          name: "description",
+          content:
+            "TN Fulfillment is the all-in-one solution for POD and dropshipping sellers",
+        },
+      ],
+    },
+  },
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  css: ["~/assets/css/tailwind.css"],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  modules: ["shadcn-nuxt", "@nuxtjs/color-mode", "@nuxtjs/i18n"],
+  colorMode: {
+    classSuffix: "",
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+  i18n: {
+    lazy: true,
+    locales: [
+      { code: "en", language: "en-US", file: "en.json", name: "english" },
+      { code: "vi", language: "vi-VN", file: "vi.json", name: "vietnamese" },
+    ],
+    defaultLocale: "en",
+    langDir: "locales/",
+    strategy: "no_prefix",
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,116 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Icon } from "@iconify/vue";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const colorMode = useColorMode();
+
+const { locale, locales, setLocale } = useI18n();
+
+const changeLang = async (lang) => {
+  await setLocale(lang);
+};
+</script>
+<template>
+  <Card class="w-[350px]">
+    <CardHeader>
+      <CardTitle>Create project</CardTitle>
+      <CardDescription>Deploy your new project in one-click.</CardDescription>
+    </CardHeader>
+    <CardContent>
+      <form>
+        <div class="grid w-full items-center gap-4">
+          <div class="flex flex-col space-y-1.5">
+            <Label for="name">Name</Label>
+            <Input id="name" placeholder="Name of your project" />
+          </div>
+          <div class="flex flex-col space-y-1.5">
+            <Label for="framework">Framework</Label>
+            <Select>
+              <SelectTrigger id="framework">
+                <SelectValue placeholder="Select" />
+              </SelectTrigger>
+              <SelectContent position="popper">
+                <SelectItem value="next"> Next.js </SelectItem>
+                <SelectItem value="sveltekit"> SvelteKit </SelectItem>
+                <SelectItem value="astro"> Astro </SelectItem>
+                <SelectItem value="nuxt"> Nuxt </SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+      </form>
+    </CardContent>
+    <CardFooter class="flex justify-between">
+      <Button variant="outline"> Cancel </Button>
+      <Button>Deploy</Button>
+    </CardFooter>
+  </Card>
+
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="outline">
+        <Icon
+          icon="radix-icons:moon"
+          class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+        />
+        <Icon
+          icon="radix-icons:sun"
+          class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+        />
+        <span class="sr-only">Toggle theme</span>
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end">
+      <DropdownMenuItem @click="colorMode.preference = 'light'">
+        Light
+      </DropdownMenuItem>
+      <DropdownMenuItem @click="colorMode.preference = 'dark'">
+        Dark
+      </DropdownMenuItem>
+      <DropdownMenuItem @click="colorMode.preference = 'system'">
+        System
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+
+  <Select>
+    <SelectTrigger id="framework">
+      <SelectValue placeholder="Select" />
+    </SelectTrigger>
+    <SelectContent position="popper">
+      <SelectItem v-for="item in locales" :value="item.code">
+        {{ item.language }}
+      </SelectItem>
+    </SelectContent>
+  </Select>
+  <Button @click="changeLang('vi')">Click</Button>
+
+  {{ $t("hello") }}
+
+  <Button>Button</Button>
+  {{ locale }}
+  <SharedSidebar />
+</template>
